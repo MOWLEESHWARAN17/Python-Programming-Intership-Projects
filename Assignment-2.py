c677508f9c3c99aedc965a4dc03db9bef70c797e
Mowleeshwaran V R <100144114+MOWLEESHWARAN17@users.noreply.github.com>
2023-04-17 17:17:59 +0530

    Create Assignment-2.py

---
+++ Assignment-2.py
@@ -0,0 +1,10 @@
+Write a program to find the sum and average of 3 numbers.
+
+sum1=0
+avg=0
+for i in range(0,3):
+  n=int(input())
+  sum1=sum1+n
+print("THE SUM IS ",sum1)
+avg=sum1/3
+print("THE AVERAGE IS ",avg)  
