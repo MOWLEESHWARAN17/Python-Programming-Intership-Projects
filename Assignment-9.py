862327709958482fad7ca398772956f1d06c375a
Mowleeshwaran V R <100144114+MOWLEESHWARAN17@users.noreply.github.com>
2023-04-17 19:31:21 +0530

    Create Assignment-9.py

---
+++ Assignment-9.py
@@ -0,0 +1,10 @@
+#Write a function to generate the Fibonacci series and call the function.
+
+# Call the function to generate Fibonacci series up to 10 terms
+n = 10
+fib_series = generate_fibonacci_series(n)
+
+# Print the generated Fibonacci series
+print("Fibonacci series up to {} terms:".format(n))
+print(fib_series)
+
