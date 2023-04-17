2e298d0e8af61a20754232e46b23fac45d5c678d
Mowleeshwaran V R <100144114+MOWLEESHWARAN17@users.noreply.github.com>
2023-04-17 19:27:06 +0530

    Create Assignment-6.py

---
+++ Assignment-6.py
@@ -0,0 +1,21 @@
+#Create a tuple t1=(10,20,30,40,30,50) and perform the following 
+#i. Find the index of number 40 
+#ii. Count for the number 30 
+#iii. Delete the tuple.
+
+
+# Create a tuple
+t1 = (10, 20, 30, 40, 30, 50)
+
+# Find the index of number 40
+index_40 = t1.index(40)
+print("Index of 40:", index_40)
+
+# Count for the number 30
+count_30 = t1.count(30)
+print("Count of 30:", count_30)
+
+# Delete the tuple
+del t1
+
+
